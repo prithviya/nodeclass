b420f41bd6cf6053a5a1cf73ea8baad16d5f3b6c
prithviya <geethaa14367@gmail.com>
2025-05-19 17:14:24 +0530

    4 commit

--- day4 - task/app.js
+++ day4 - task/app.js
@@ -1,14 +1,21 @@
-import express from 'express';
-import { getAllUsers, getUserById } from './utils.js';
+import express from "express";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  putUser,
+  patchUser,
+  delUser,
+} from "./utils.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   const { name } = req.query;
-  
+
   let users = await getAllUsers();
 
   if (name) {
@@ -19,19 +26,21 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
-app.get('/users/:id', async (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const user = await getUserById(req.params.id);
   if (!user) {
-    return res.status(404).json({ error: 'User not found' });
+    return res.status(404).json({ error: "User not found" });
   }
   res.json(user);
 });
 
-app.get('/users/search', async (req, res) => {
+app.get("/users/search", async (req, res) => {
   const { names } = req.query;
 
   if (!names) {
-    return res.status(400).json({ error: 'Query parameter "names" is required' });
+    return res
+      .status(400)
+      .json({ error: 'Query parameter "names" is required' });
   }
 
   const nameArray = Array.isArray(names) ? names : [names];
@@ -42,6 +51,40 @@ app.get('/users/search', async (req, res) => {
   );
 
   res.json(matchedUsers);
+});
+
+app.post("/user", async (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email)
+    return res.status(400).json({ error: "Name and email are required" });
+
+  const newUser = await createUser({ name, email });
+  res.status(201).json(newUser);
+});
+
+app.put("/putuser", async (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email)
+    return res.status(400).json({ error: "Name and email are required" });
+
+  const newUser = await putUser();
+  res.status(201).json(newUser);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, email } = req.body;
+  if (!name || !email)
+    return res.status(400).json({ error: "Name and email are required" });
+
+  const newUser = await patchUser(id, name, email);
+  res.status(201).json(newUser);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const newUser = await delUser(id);
+  res.status(201).json(newUser);
 });
 
 app.listen(port, () => {
--- day4 - task/utils.js
+++ day4 - task/utils.js
@@ -1,32 +1,32 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 console.log(__filename);
 const __dirname = path.dirname(__filename);
 console.log(__dirname);
-const usersDir = path.join(__dirname, 'users');
+const usersDir = path.join(__dirname, "users");
 console.log(usersDir);
+let id = 3;
 
 async function ensureUsersDir() {
   try {
     await fs.mkdir(usersDir);
   } catch (err) {
-    if (err.code !== 'EEXIST') throw err;
+    if (err.code !== "EEXIST") throw err;
   }
 }
 
 export async function getAllUsers() {
   await ensureUsersDir();
   const files = await fs.readdir(usersDir);
-  console.log(files)
+  console.log(files);
   const users = await Promise.all(
     files.map(async (file) => {
-      const content = await fs.readFile(path.join(usersDir, file), 'utf-8');
-      console.log(content)
+      const content = await fs.readFile(path.join(usersDir, file), "utf-8");
+      console.log(content);
       return JSON.parse(content);
-
     })
   );
   return users;
@@ -35,8 +35,48 @@ export async function getAllUsers() {
 export async function getUserById(id) {
   const filePath = path.join(usersDir, `${id}.json`);
   try {
-    const content = await fs.readFile(filePath, 'utf-8');
+    const content = await fs.readFile(filePath, "utf-8");
     return JSON.parse(content);
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function createUser() {
+  const filePath = path.join(usersDir, `${++id}.json`);
+  try {
+    const file = await fs.writeFile(
+      `${filePath}`,
+      `{ "id": ${id}, "name": "devi", "email": "devi@example.com"}`
+    );
+    return file;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function putUser() {
+  let a = 10;
+  console.log(a);
+}
+
+export async function patchUser(id, name, email) {
+  const filePath = path.join(usersDir, `${id}.json`);
+  try {
+    const file = await fs.writeFile(
+      `${filePath}`,
+      `{ "id": ${id}, "name": "${name}", "email": "${email}"}`
+    );
+    return file;
+  } catch (err) {
+    return null;
+  }
+}
+export async function delUser(id) {
+  const filePath = path.join(usersDir, `${id}.json`);
+  try {
+    const file = await fs.unlink(`${filePath}`);
+    return file;
   } catch (err) {
     return null;
   }
